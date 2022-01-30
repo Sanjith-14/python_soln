f996290db9d1091c2be8c5d6fe3d7789295f591e
Sanjith S <71712730+Sanjith-14@users.noreply.github.com>
2022-01-30 19:28:36 +0530

    Create Check_subset.py

---
+++ Check_subset.py
@@ -0,0 +1,9 @@
+for i in range(int(input())): 
+    a = int(input())
+    A = set(input().split()) 
+    b = int(input())
+    B = set(input().split())
+    if (A.issubset(B)):
+        print("True")
+    else:
+        print("False")
